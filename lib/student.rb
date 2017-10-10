d8cc5925a9ca4a442eb88ad1fda2ca925b7b3a1d
Christine Tran <christinetran825@gmail.com>
2017-10-10 04:37:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,10 +88,10 @@ class Student
       WHERE grade = 10
       LIMIT = 1
     SQL
-    #
-    # DB[:conn].execute(sql).map do |row|
-    #   self.new_from_db(row)
-    # end.first
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.all_students_in_grade_X(x)
