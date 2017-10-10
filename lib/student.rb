e4bfae4b7e1d32d40697dc5500519c67c1429a21
Christine Tran <christinetran825@gmail.com>
2017-10-10 03:56:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,27 +3,42 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+
   end
+
+
+  # describe '.new_from_db' do
+  #   it 'creates an instance with corresponding attribute values' do
+  #     row = [1, "Pat", 12]
+  #     pat = Student.new_from_db(row)
+  #
+  #     expect(pat.id).to eq(row[0])
+  #     expect(pat.name).to eq(row[1])
+  #     expect(pat.grade).to eq(row[2])
+  #   end
+  # end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
