8c8fee35bca813b918173cb166b23b7e47616285
Christine Tran <christinetran825@gmail.com>
2017-10-10 04:15:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,6 +73,24 @@ class Student
   def self.first_X_students_in_grade_10
 
   end
+  # 
+  # describe '.first_X_students_in_grade_10' do
+  #   it 'returns an array of the first X students in grade 10' do
+  #
+  #     pat.name = "Pat"
+  #     pat.grade = 10
+  #     pat.save
+  #     sam.name = "Sam"
+  #     sam.grade = 10
+  #     sam.save
+  #     jess.name = "Jess"
+  #     jess.grade = 10
+  #     jess.save
+  #
+  #     first_X_students = Student.first_X_students_in_grade_10(2)
+  #     expect(first_X_students.size).to eq(2)
+  #   end
+  # end
 
   def self.first_student_in_grade_10
 
