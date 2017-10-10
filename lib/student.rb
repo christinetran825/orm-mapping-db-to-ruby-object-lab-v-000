3292244e95b0ec4aa1b3fce00215bbd2fc7d84a9
Christine Tran <christinetran825@gmail.com>
2017-10-10 03:57:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,18 +10,6 @@ class Student
     new_student
   end
 
-
-  # describe '.new_from_db' do
-  #   it 'creates an instance with corresponding attribute values' do
-  #     row = [1, "Pat", 12]
-  #     pat = Student.new_from_db(row)
-  #
-  #     expect(pat.id).to eq(row[0])
-  #     expect(pat.name).to eq(row[1])
-  #     expect(pat.grade).to eq(row[2])
-  #   end
-  # end
-
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
